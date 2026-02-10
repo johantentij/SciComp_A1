4235ef1c2eef19b7e80a551688923789a7202500
fsedawd03 <139582131+fsedawd03@users.noreply.github.com>
2026-02-11 00:34:25 +0100

    created the methods using njit and their parallel counterparts
    Signed-off-by: fsedawd03 <139582131+fsedawd03@users.noreply.github.com>

--- 1.1/euler_methods.py
+++ 1.1/euler_methods.py
@@ -1,79 +1,161 @@
 import numpy as np
 from typing import Any, Union
 import numpy.typing as npt
+from numba import njit
+import numexpr as ne
+
+
+@njit
+def __euler_forward(psi, y, equations_matrix, dx, dt, c, time_steps):
+    for niter in range(time_steps):
+        d2psi_dx2 = equations_matrix.dot(np.ascontiguousarray(psi[:, niter])) / dx ** 2
+
+        psi[:, niter + 1] = psi[:, niter] + dt * y[:, niter]
+        y[:, niter + 1] = y[:, niter] + dt * c ** 2 * d2psi_dx2
+
+@njit
+def __runge_kutta4(psi, y, equations_matrix, dx, dt, c, time_steps):
+    # Runge-Kutta 4
+    for niter in range(time_steps):
+        k1_psi = y[:, niter]
+        k1_y = c ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter])) / dx ** 2
+
+        k2_psi = y[:, niter] + 0.5 * dt * k1_y
+        k2_y = c ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter]) + 0.5 * dt * k1_psi) / dx ** 2
+
+        k3_psi = y[:, niter] + 0.5 * dt * k2_y
+        k3_y = c ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter]) + 0.5 * dt * k2_psi) / dx ** 2
+
+        k4_psi = y[:, niter] + dt * k3_y
+        k4_y = c ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter]) + dt * k3_psi) / dx ** 2
+
+        psi[:, niter + 1] = psi[:, niter] + dt / 6 * (k1_psi + 2 * k2_psi + 2 * k3_psi + k4_psi)
+        y[:, niter + 1] = y[:, niter] + dt / 6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y)
+
+@njit
+def __stormer_verlet(psi, y, equations_matrix, dx, dt, c, time_steps):
+    # Størmer–Verlet method
+    psi[:, 1] = psi[:,0]
+    for niter in range(2, time_steps + 1):
+        psi[:, niter] = (2 * psi[:, niter - 1] - psi[:, niter - 2] +
+                         c * (dt / dx) ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter - 1])))
+
+@njit
+def __leapfrog(psi, y, equations_matrix, dx, dt, c, time_steps):
+    # Leapfrog method
+    for niter in range(time_steps):
+        d2psi_dx2 = equations_matrix.dot(np.ascontiguousarray(psi[:, niter])) / dx ** 2
+        psi[:, niter + 1] = psi[:, niter] + dt * y[:, niter] + 0.5 * c ** 2 * d2psi_dx2 * dt ** 2
+
+        d2psi_dx2_next = equations_matrix.dot(np.ascontiguousarray(psi[:, niter + 1])) / dx ** 2
+        y[:, niter + 1] = y[:, niter] + 0.5 * (d2psi_dx2 + d2psi_dx2_next) * dt
+
+##
+## Parallel version of the above functions
+##
+
+def __euler_forward_paral(psi, y, equations_matrix, dx, dt, c, time_steps):
+    for niter in range(time_steps):
+        d2psi_dx2 = equations_matrix.dot(np.ascontiguousarray(psi[:, niter])) / dx ** 2
+
+        ## Needed for numexpr package as it does not understand pythonic array splicing
+        psi_n = psi[:,niter]
+        y_n = y[:,niter]
+
+        psi[:, niter + 1] = ne.evaluate('psi_n + dt * y_n')
+        y[:, niter + 1] = ne.evaluate('y_n + dt * c ** 2 * d2psi_dx2')
+
+def __runge_kutta4_paral(psi, y, equations_matrix, dx, dt, c, time_steps):
+    # Runge-Kutta 4
+    for niter in range(time_steps):
+
+        ## Needed for numexpr package as it does not understand pythonic array splicing
+        psi_n = psi[:, niter]
+        y_n = y[:, niter]
+
+        k1_psi = y[:, niter]
+        k1_y = c ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter])) / dx ** 2
+
+        k2_psi = y[:, niter] + 0.5 * dt * k1_y
+        k2_y = c ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter]) + 0.5 * dt * k1_psi) / dx ** 2
+
+        k3_psi = y[:, niter] + 0.5 * dt * k2_y
+        k3_y = c ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter]) + 0.5 * dt * k2_psi) / dx ** 2
+
+        k4_psi = y[:, niter] + dt * k3_y
+        k4_y = c ** 2 * equations_matrix.dot(np.ascontiguousarray(psi[:, niter]) + dt * k3_psi) / dx ** 2
+
+        psi[:, niter + 1] = ne.evaluate('psi_n + dt / 6 * (k1_psi + 2 * k2_psi + 2 * k3_psi + k4_psi)')
+        y[:, niter + 1] = ne.evaluate('y_n + dt / 6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y)')
+
+def __stormer_verlet_paral(psi, y, equations_matrix, dx, dt, c, time_steps):
+    # Størmer–Verlet method
+    psi[:, 1] = psi[:,0]
+    for niter in range(2, time_steps + 1):
+        d2psi_dx2 = equations_matrix.dot(np.ascontiguousarray(psi[:, niter - 1]))
+
+        ## Needed for numexpr package as it does not understand pythonic array splicing
+        psi_n = psi[:, niter - 1]
+        psi_n_prev = psi[:, niter - 2]
+
+        psi[:, niter] = ne.evaluate('2 * psi_n - psi_n_prev + \
+                         c * (dt / dx) ** 2 * d2psi_dx2')
+
+def __leapfrog_paral(psi, y, equations_matrix, dx, dt, c, time_steps):
+    # Leapfrog method
+    for niter in range(time_steps):
+        d2psi_dx2 = equations_matrix.dot(np.ascontiguousarray(psi[:, niter])) / dx ** 2
+
+        ## Needed for numexpr package as it does not understand pythonic array splicing
+        y_n = y[:,niter]
+        psi_n = psi[:,niter]
+
+        psi[:, niter + 1] = ne.evaluate('psi_n + dt * y_n + 0.5 * c ** 2 * d2psi_dx2 * dt ** 2')
+
+        d2psi_dx2_next = equations_matrix.dot(np.ascontiguousarray(psi[:, niter + 1])) / dx ** 2
+        y[:, niter + 1] = ne.evaluate('y_n + 0.5 * (d2psi_dx2 + d2psi_dx2_next) * dt')
+
+APPROX_METHOD = {
+    "EF": __euler_forward,
+    "RK4": __runge_kutta4,
+    "LP": __leapfrog,
+    "SV": __stormer_verlet,
+}
+
+APPROX_METHOD_PARALLEL = {
+    "EF": __euler_forward_paral,
+    "RK4": __runge_kutta4_paral,
+    "LP": __leapfrog_paral,
+    "SV": __stormer_verlet_paral,
+}
 
 def approx_wave(psi_0:npt.NDArray, time_steps: Union[int, np.integer[Any]],
-                mesh_points: npt.NDArray,
                 equations_matrix: npt.NDArray,
                 dt: Union[int, float],
                 c: Union[int,float],
                 dx: Union[int,float],
-                N:int,
-                method:str ="EF") -> npt.NDArray:
+                N: int,
+                method: str="EF",
+                threads: int=None) -> npt.NDArray:
 
-    def euler_forward():
-        # Euler forward method
-        for niter in range(time_steps):
-            d2psi_dx2 = equations_matrix.dot(psi[:, niter]) / dx ** 2
-
-            psi[:, niter + 1] = psi[:, niter] + dt * y[:, niter]
-            y[:, niter + 1] = y[:, niter] + dt * c ** 2 * d2psi_dx2
-
-    def runge_kutta4():
-        # Runge-Kutta 4
-        for niter in range(time_steps):
-            k1_psi = y[:, niter]
-            k1_y = c ** 2 * equations_matrix.dot(psi[:, niter]) / dx ** 2
-
-            k2_psi = y[:, niter] + 0.5 * dt * k1_y
-            k2_y = c ** 2 * equations_matrix.dot(psi[:, niter] + 0.5 * dt * k1_psi) / dx ** 2
-
-            k3_psi = y[:, niter] + 0.5 * dt * k2_y
-            k3_y = c ** 2 * equations_matrix.dot(psi[:, niter] + 0.5 * dt * k2_psi) / dx ** 2
-
-            k4_psi = y[:, niter] + dt * k3_y
-            k4_y = c ** 2 * equations_matrix.dot(psi[:, niter] + dt * k3_psi) / dx ** 2
-
-            psi[:, niter + 1] = psi[:, niter] + dt / 6 * (k1_psi + 2 * k2_psi + 2 * k3_psi + k4_psi)
-            y[:, niter + 1] = y[:, niter] + dt / 6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y)
-
-    def stormer_verlet():
-        # Størmer–Verlet method
-        psi[:, 1] = psi_0
-        for niter in range(2, time_steps + 1):
-            psi[:, niter] = (2 * psi[:, niter - 1] - psi[:, niter - 2] +
-                             c * (dt / dx) ** 2 * equations_matrix.dot(psi[:, niter - 1]))
-
-    def leapfrog():
-        # Leapfrog method
-        # nonlocal psi,y ???somehow this is not needed???
-        for niter in range(time_steps):
-            d2psi_dx2 = equations_matrix.dot(psi[:, niter]) / dx ** 2
-            psi[:, niter + 1] = psi[:, niter] + dt * y[:, niter] + 0.5 * c ** 2 * d2psi_dx2 * dt ** 2
-
-            d2psi_dx2_next = equations_matrix.dot(psi[:, niter + 1]) / dx ** 2
-            y[:, niter + 1] = y[:, niter] + 0.5 * (d2psi_dx2 + d2psi_dx2_next) * dt
-
-    APPROX_METHOD = {
-        "EF": euler_forward,
-        "RK4": runge_kutta4,
-        "LP": leapfrog,
-        "SV": stormer_verlet,
-    }
-
-    operation = APPROX_METHOD.get(method.upper())
+    if threads:
+        operation = APPROX_METHOD_PARALLEL.get(method.upper())
+        ne.set_num_threads(threads)
+    else:
+        operation = APPROX_METHOD.get(method.upper())
 
     if operation:
         psi_0 = psi_0[1:N]
         psi = np.empty((N-1,time_steps+1), dtype=np.float64)
         psi[:, 0] = psi_0
+        y = None
 
         if method.upper() != "SV":
             y = np.empty((N-1,time_steps+1), dtype=np.float64)
             y[:,0] = np.zeros(N-1,dtype=np.float64)
 
-        # Function to generate the aproximation
-        operation()
+        # Function to generate the approximation
+        operation(psi, y, equations_matrix, dx, dt, c, time_steps)
 
         psi = psi[:, 1:]
         psi = np.insert(psi, (0, N - 1), np.zeros(time_steps), axis=0)
